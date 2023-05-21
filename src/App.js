3526d3bcedce74eb9b585d1739475366d7774b1e
Hyeon(Hena) Park <93799006+Hena1234@users.noreply.github.com>
2023-05-20 23:23:56 -0400

    ExpenseDate and Item with CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Expenses from "./components/Expenses";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 8, 28),
+    },
+    { id: "e2", title: "New Tv", amount: 294.67, date: new Date(2020, 11, 2) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.48,
+      date: new Date(2021, 1, 14),
+    },
+    {
+      id: "e4",
+      title: "New Desk(Wooden)",
+      amount: 450,
+      date: new Date(2021, 6, 22),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
-
 export default App;
