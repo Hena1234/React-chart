28f3dc2139996d8cb157078075ab27f79bcdfce4
Hyeon(Hena) Park <93799006+Hena1234@users.noreply.github.com>
2023-05-21 22:27:07 -0400

    Add NewExpensem, ExpenseForm and ExpenseItem with CSS

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // 2. setUserInput({
+    //   ...userInput,
+    //   enterTitle: event.target.value,
+    // });
+    // 3. setUserInput((prevState) => {
+    //   //prevState: 이전상태의 값 변수 (enteredTitle, enteredAmount, enteredDate)
+    //   // 이전의 값과 받은값(새로운 값)을 return한다.
+    //   //이전 상태 값을 가져와서 일부 속성을 변경한 후
+    //   //새로운 상태를 설정하는 것을 의미합니다. 이를 통해 이전 상태의 속성 값들을 그대로 유지하면서 특정 속성만 업데이트할 수 있습니다.
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // 2. setUserInput({
+    //   ...userInput,
+    //   enterAmount: event.target.value,
+    // });
+    // 3. setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // 2.setUserInput({
+    //   ...userInput,
+    //   enterDate: event.target.value,
+    // });
+    // 3. setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      data: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2020-01-01"
+            max="2023-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+export default ExpenseForm;
